3d29d8830d6275fd5fd3541b8fa1948d810f9e1d
Ishitha <ishithasabbineni@gmail.com>
2024-12-15 14:04:07 -0500

    Update TechiesDriveBase.py
    Added move while moving arm function

--- TechiesDriveBase.py
+++ TechiesDriveBase.py
@@ -2,22 +2,25 @@
 #     For team 37633     # 
 #        DriveBase       #
 ##########################
-
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait, StopWatch, multitask, run_task
+from TechiesArm import *
 
 hub = PrimeHub()
 
 left_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.D)
 color_sensor = ColorSensor(Port.E)
-color_sensor_side = ColorSensor(Port.B)
 
 drive_base = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=110)
 drive_base.use_gyro(True)
+
+def IsForceSensorPressed():
+    force = ForceSensor(Port.B)
+    return force.pressed()
 
 def SetGyro(truefalse):
     drive_base.use_gyro(truefalse)
@@ -26,14 +29,18 @@ def SetSpeed(speed):
     drive_base.settings(straight_speed=speed)
 
 def SetAccel(accelStraight, accelTurn):
-    drive_base.settings(straight_acceleration=accelStraight, turn_acceleration=400)
+    drive_base.settings(straight_acceleration=accelStraight, turn_acceleration=accelTurn)
    
 def MoveForward(distance):
     drive_base.straight(distance)
 
 def MoveForwardWithAccel(distance, accelStraight, accelTurn):
-    drive_base.settings(straight_acceleration=accelStraight, turn_acceleration=400)
+    drive_base.settings(straight_acceleration=accelStraight, turn_acceleration=accelTurn)
     drive_base.straight(distance)
+
+def MoveBackwardWithAccel(distance, accelStraight, accelTurn):
+    drive_base.settings(straight_acceleration=accelStraight, turn_acceleration=accelTurn)
+    drive_base.straight(-1 * distance)
 
 def MoveBackward(distance):
     drive_base.straight(-1* distance)
@@ -67,3 +74,9 @@ def MoveForwardWithSuddenStop(distance):
         print(drive_base.distance())
         wait(20)
     drive_base.straight(0)
+
+def move_and_leftdown(robot_distance, SetSpeed, armspeed, angle):
+    left_motor.run(SetSpeed) 
+    right_motor.run(SetSpeed) 
+    left_arm.run_target(armspeed, -1 * angle)
+    MoveForward(robot_distance)
