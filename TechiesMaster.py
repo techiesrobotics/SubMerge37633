458163548d788b5a2630c832b13cdf1d10e6eccf
Techies Robotics <leoleoleo11912@gmail.com>
2024-12-17 11:31:29 -0500

    Update TechiesMaster.py
    Update Master to add color light blue, replace white

--- TechiesMaster.py
+++ TechiesMaster.py
@@ -11,41 +11,37 @@ from shippinglanes_octo_segments import *
 from sonar_submersible_fish import *
 from Run_TridentShark import *
 
-ORANGE_HIGH = 20
-ORANGE_LOW = 1
 RED_HIGH = 365
 RED_LOW = 349
+ORANGE_HIGH = 20
+ORANGE_LOW = 1
 MAGENTA_HIGH = 342
 MAGENTA_LOW = 330
+
 BLUE_HIGH = 220
 BLUE_LOW = 210
+LIGHT_BLUE_HIGH = 200
+LIGHT_BLUE_LOW = 190
 PURPLE_HIGH = 248
 PURPLE_LOW = 230
-DARK_GREEN_HIGH = 165
-DARK_GREEN_LOW = 145
-LIGHT_GREEN_HIGH = 90
-LIGHT_GREEN_LOW = 100
+
+# may not need
+#DARK_GREEN_HIGH = 165
+#DARK_GREEN_LOW = 145
+#YELLOW_HIGH = 60
+#YELLOW_LOW = 41
 
 hub = PrimeHub()
 while True:
     detectedHSV = color_sensor_side.hsv()
     detectedColor = color_sensor_side.color()
-    print(detectedColor, detectedHSV)
+    #print(detectedColor, detectedHSV)
 
-    #TODO:  when to run the mask mission?  
-    # Do_MastAndHangTree
     if(detectedColor == Color.GREEN):
         print("==============DarkGreen, Do_CollectLeftTree")
         Do_CollectLeftTree()
     
-    elif detectedColor == Color.YELLOW:  # TODO need to test
-        #if(detectedHSV.h > LIGHT_GREEN_LOW and detectedHSV.h < LIGHT_GREEN_HIGH):
-            print("==============YELLOW, Do_HangDiverSeabedCollectRight")
-            Do_HangDiverSeabedCollectRight()
-
-    elif detectedColor == Color.WHITE:  # TODO Need to use a different color
-            print("==============YELLOW, Do_MastHangTree")
-            Do_MastHangTree()
+    # next is Do_MastHangTree(), move it to light blue
 
     elif detectedColor == Color.RED:
         if(detectedHSV.h > RED_LOW and detectedHSV.h < RED_HIGH):
@@ -56,25 +52,29 @@ while True:
             tridentshark()
         elif(detectedHSV.h > MAGENTA_LOW and detectedHSV.h < MAGENTA_HIGH): #TODO
             print("==============MAGENTA, Run_Habitat & sample to boat")
-            #Add the run for the Artificial Habitat
+            #Add the run for the Artificial Habitat & dump sample to boat
         else:
             print("Not Orange, Red, or Magenta")
             print(color_sensor_side.color())
-    
-    # right side runs
+
+    elif detectedColor == Color.YELLOW:  # move from left to right
+        print("==============YELLOW, Do_HangDiverSeabedCollectRight")
+        Do_HangDiverSeabedCollectRight()
+
+    # right side runs + replace white to run mastHangTree
     elif detectedColor == Color.BLUE:
-        if(detectedHSV.h > BLUE_LOW and detectedHSV.h < BLUE_HIGH):
+        if(detectedHSV.h > LIGHT_BLUE_LOW and detectedHSV.h < LIGHT_BLUE_HIGH):
+            print("==============light blue, Do_MastAndHangTree")
+            Do_MastAndHangTree()
+        elif(detectedHSV.h > BLUE_LOW and detectedHSV.h < BLUE_HIGH):
             print("==============BLUE, shippinglanes_octo_segments")
             shippinglanes_octo_segments() #TODO add pause mission inbetween
-        if(detectedHSV.h > PURPLE_LOW and detectedHSV.h < PURPLE_HIGH):
+        elif(detectedHSV.h > PURPLE_LOW and detectedHSV.h < PURPLE_HIGH):
             print("==============PURPLE, sonar_submersible_fish")
             sonar_submersible_fish()
         else:
-            print("========================================too close to tell")
+            print("======================too close to tell")
     wait(100)
-'''
-    elif color_sensor_side.color() == Color.VIOLET:  # TODO
-        print(color_sensor_side.color())
-'''
+
         
 
