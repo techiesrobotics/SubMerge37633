db488a2162e82525987de47526f33e6e43c731d7
Leo <leoleoleo11912@gmail.com>
2024-12-23 14:22:58 -0500

    Update TechiesArm.py

--- TechiesArm.py
+++ TechiesArm.py
@@ -3,7 +3,6 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, multitask, run_task
-from TechiesArm import *
 
 left_arm = Motor(Port.A)
 right_arm = Motor(Port.F, Direction.COUNTERCLOCKWISE)
