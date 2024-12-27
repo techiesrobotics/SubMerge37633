7e2a69701c4a99688d530fb1f5264d003f696630
Leo <leoleoleo11912@gmail.com>
2024-12-27 18:21:33 -0500

    Update Run_MastHangTree.py

--- Run_MastHangTree.py
+++ Run_MastHangTree.py
@@ -4,48 +4,47 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 from TechiesDriveBase import *
-from TechiesArm import *
 
-##########################
-# For Team 37633 Techies #
-#  Run_MastHangTree.py   #
-##########################
+####################################
+#      For Team 37633 Techies      #
+#   Run_MastHangDiverReefShark.py  #
+####################################
 
-hub = PrimeHub()
-'''
-SetSpeed(350)
-drive_base.curve(485,90)
-drive_base.curve(-420,100)
-'''
 def mastKraken():
     #This function completes the "Raising the Mast" and "Kraken's Treasure" Missions
     SetSpeed(900)
-    MoveForward(250)
     TurnRight(30)
-    MoveForward(295)
-    SetSpeed(250)
+    MoveForward(450)
+    SetSpeed(100)
     TurnRight(60)
-    SetSpeed(300)
-    MoveForward(325)
-    MoveBackward(10)
-    MoveForward(50)
-    MoveBackward(110)
     SetSpeed(900)
-    TurnLeft(60)
-    MoveBackward(525)
+    MoveForward(450)
+    MoveBackward(20)
+    MoveForward(60)
+    SetSpeed(200)
+    MoveBackward(140)
     
+def hangDiverandHitCoralReef(): # hang diver and hit coral bud
+    TurnRight(103)
+    MoveBackward(213)
+    right_arm.run_until_stalled(200, duty_limit=30)
+    MoveRightArmDown(200, 30)
+    MoveForward(160)
+    TurnLeft(55)
 
-def hangTree():
-    #This function hangs the coral tree
-    MoveForward(515)
-    MoveLeftArmDown(900,35)
-    MoveBackward(515)
-    MoveLeftArmUp(900,35)
+def doShark():
+    MoveBackward(190)
+    right_arm.run_until_stalled(200, duty_limit=50)
+    MoveRightArmDown(200, 20)
+    MoveForward(140)
+    TurnRight(55)
+    SetSpeed(900)
+    MoveForward(800)
 
-def Do_MastAndHangTree():
+def Do_MastDiverReefShark():
     #This function calls both individual mission functions and combines them
     mastKraken()
-    PauseMission()
-    hangTree()
+    hangDiverandHitCoralReef()
+    doShark()
     
 #Do_MastAndHangTree()
