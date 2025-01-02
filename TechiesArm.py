e6eeebb7c9e714794896130efdde49ff0ffd50af
Leo <leoleoleo11912@gmail.com>
2025-01-01 21:02:07 -0500

    Update TechiesArm.py
    Updated comments and added function

--- TechiesArm.py
+++ TechiesArm.py
@@ -1,3 +1,10 @@
+###################################
+#         For team 37633          # 
+#         TechiesArm.py           #
+#    This file defines the arm    #
+#        movement functions       #
+###################################
+
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
@@ -19,9 +26,11 @@ def MoveRightArmUp(speed, angle):
 def MoveRightArmDown(speed, angle):
     right_arm.run_angle(speed, angle * -1)
 
+#The asynchronous function allows two motors to run at the same time.
+
 async def moveArmUp(arm, speed, angle):
     await arm.run_angle(speed, angle)
-    
+
 async def moveArmDown(arm, speed, angle):
     await arm.run_angle(speed, -1 * angle)
     
@@ -31,17 +40,36 @@ async def moveBothArmUp(speed, angle):
 async def moveBothArmDown(speed, angle):
     await multitask(moveArmDown(left_arm, speed, angle), moveArmDown(right_arm, speed, angle))
 
+# This function detects the stall on the motor to tell if it gets stuck
+# The motor continues to run until it reaches the desired angle on surpasses the max load.
+# When the function is called, we must type in the max load based on testing.
+# We test the load on the motor when there is no stall and code the program to print the load.
+# Then we add strain on the motor to see what the load is when it stalls.
+# That number will be our max load.
 def DetectArmStall(arm, speed, angle, maxLoad):
     arm.reset_angle(0)
     arm.run(angle)
     while abs(arm.load()) < abs(maxLoad):
+        print("======", arm.load())
+
         if abs(arm.angle()) < abs(angle):
-            print(arm.angle())
             wait(1)
         else:
             break
     arm.hold()
-#DetectArmStall(left_arm,150,90,40)
+
+# This function detects stall by starting a watch when the arm starts moving.
+# When the needed angle is reached or the watch reaches a specific time, the program continues.
+# The number for the watch is important because if it is too small, the arm won't have enough time to reach it's angle. But if it's too big, it will waste too much time.
+def MoveArmWithStallTimeDetection(arm, speed, angle):
+    arm.reset_angle(0)
+    arm.run(angle)
+    watch = StopWatch()
+    while abs(arm.angle() < abs(angle)):
+        if watch.time() > 700:
+            break
+    arm.hold()
+
 #MoveLeftArmUp(100, 90)
 #MoveLeftArmDown(100, 90)
 
@@ -50,3 +78,9 @@ def DetectArmStall(arm, speed, angle, maxLoad):
 
 #run_task(moveBothArmUp(100, 90))
 
+#MoveArmWithStallTimeDetection(right_arm, 200, 90)
+
+#MoveLeftArmDown(80, 170)
+#MoveLeftArmUp(80, 170)
+
+#MoveArmWithStallTimeDetection(right_arm, 150, -90)
