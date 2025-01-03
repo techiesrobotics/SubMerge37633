11cbdc9f9ca8b022bb7dc0cfa13107815b4c71ad
Ishitha <ishithasabbineni@gmail.com>
2025-01-03 12:42:29 -0500

    Update sonar_submersible_fish.py

--- sonar_submersible_fish.py
+++ sonar_submersible_fish.py
@@ -14,36 +14,30 @@ def sonar():
     MoveBackward(190)
 
 def submersible():
+    AccelDefaultReset()
     TurnRight(30)
     MoveBackward(100)
     TurnLeft(30)
     MoveRightArmDown(450, 90)
-    run_task(DriveForwardAndMoveArm(230, left_arm, 210, 100))
+    run_task(DriveForwardAndMoveArm(320, left_arm, 210, -135))
     TurnRight(45)
-    MoveForward(40)
+    MoveForward(55)
     wait(10)
     StopAtWhite(290)
-    DetectArmStall(left_arm, 500, 100, 40)# The old angle was -15, TODO
+    #DetectArmStall(left_arm, 500, 1500, 40)# The old angle was -15, TODO
+    MoveArmWithStallTimeDetection(left_arm, 500, 150)
     print(left_arm.load())
 
-def fish():
-    MoveBackward(280)
-    TurnLeft(53)
-    SetSpeed(330)
-    MoveForwardWithAccel(500, 1200, 1200)
-    TurnLeft(25)
-    MoveForward(135)
-    TurnLeft(35)
-    MoveForwardWithAccel(90, 1200, 1200)
-    wait(10)
-    MoveBackward(140)
-
 def octo():
-    TurnRight(90)
     MoveBackward(180)
-    
+    TurnRight(100)
+    MoveBackward(180)
+   
 def sonar_submersible_fish():
     sonar()
     submersible()
-    fish()
     octo()
+
+sonar_submersible_fish()
+
+
