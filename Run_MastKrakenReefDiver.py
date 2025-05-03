fa3247b2a7ad25dcb9ac408560cd299ada565503
Kadenli19 <Kabobinator19@gmail.com>
2025-05-02 23:26:05 -0400

    Run_MastKrakenReefDiver.py

--- Run_MastKrakenReefDiver.py
+++ Run_MastKrakenReefDiver.py
@@ -13,45 +13,31 @@ from TechiesDriveBase import *
 def mastKraken():
     #This function completes the "Raising the Mast" and "Kraken's Treasure" Missions
     SetSpeed(900)
-    MoveRightArmDown(100 ,21)
-    TurnRight(30)
-    MoveForward(450)
+    MoveForward(190)
+    TurnRight(20)
+    MoveForward(405)
     SetSpeed(100)
-    TurnRight(60)
-    SetSpeed(900)
-    MoveForward(450)
-    MoveBackward(20)
-    MoveForward(60)
-    SetSpeed(170)
-    MoveBackward(140)
-    '''
-    SetSpeed(900)
-    TurnLeft(70)
-    MoveBackward(620)
-    '''
-    
-def hangDiverandHitCoralReef(): # hang diver and hit coral bud
-    TurnRight(102)
-    MoveBackward(213)
-    MoveRightArmUp(200, 15)
-    SetSpeed(900)
-    TurnRight(23)
-    MoveForward(810)
+    TurnRight(70)
+    SetSpeed(400)
+    MoveForward(200)
+    '''MoveBackward(20)
+    MoveForward(90)
+    SetSpeed(170)'''
+    MoveBackward(170)
 
-def doShark():
-    MoveBackward(190)
-    right_arm.run_until_stalled(200, duty_limit=50)
-    MoveRightArmDown(200, 20)
-    MoveForward(140)
-    TurnRight(35)
+def hangDiverCoralBuds():
+    #This function hangs the diver on the rack and flips the coral buds
+    SetSpeed(600)
+    TurnRight(88)
     SetSpeed(900)
-    MoveForward(800)
+    MoveBackward(510)
+  
 
 def Do_MastKrakenReefDiver():
     #This function calls both individual mission functions and combines them
     mastKraken()
-    hangDiverandHitCoralReef()
-    #doShark()
+    hangDiverCoralBuds()
+
     
-#Do_MastKrakenReefDiver()
+Do_MastKrakenReefDiver()
 
