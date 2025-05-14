b8d4cf97dfe72a6b485109f4df8c84901af75220
DJ-37633 <darsheeljha@gmail.com>
2025-05-13 20:17:17 -0400

    Add files via upload

---
+++ tridentGrey.py
@@ -0,0 +1,25 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+from TechiesDriveBase import *
+
+SetSpeed(700)
+MoveForward(200)
+TurnRight(90)
+MoveForward(340)
+TurnLeft(50.7)
+SetSpeed(400)
+MoveForward(180)
+MoveRightArmUp(100,415)
+MoveBackward(20)
+wait(300)
+MoveRightArmDown(100,315)
+SetSpeed(700)
+MoveBackward(300)
+TurnLeft(100)
+MoveForward(50)
