a46108dfb9aa4d1421fb319f9ecbc21ae85be5cd
Ishitha <ishithasabbineni@gmail.com>
2025-05-18 15:48:31 -0400

    Update shippinglanes_octo_segments.py

--- shippinglanes_octo_segments.py
+++ shippinglanes_octo_segments.py
@@ -1,31 +1,32 @@
 from TechiesDriveBase import *
+from TechiesArm import *
+
+hub = PrimeHub()
 
 def shippinglanes():
+    MoveForward(435)
+    TurnRight(25)
+    MoveForward(55)
+    wait(10)
+    MoveRightArmUp(200, 65)
+    TurnRight(30)
+    MoveBackward(140)
+    TurnRight(70)
+
+def octo():
+    MoveBackward(195)
     SetSpeed(900)
-    MoveForward(400)#415 with 2 wheels
-    wait(8)
-    TurnRight(30)
-    MoveForward(80) #60 with 2 wheels
-    MoveRightArmUp(200, 60)
-    TurnRight(30)
-    MoveBackward(180) #175 with 2 wheels
-    wait(100)
+    MoveForward(400)
 
-def unexpectedencounter():
-    TurnRight(63)
-    wait(5)
-    MoveBackward(60) #70 with 2 wheels
-    MoveForward(460)
-    SetGyro(False)
-
+'''
 def coralsegments():
     PauseMission()
     SetSpeed(100)
     MoveForward(40)
     MoveBackward(120)
+'''
 
-def shippinglanes_octo_segments():
+def shippinglanes_octo():
     shippinglanes()
-    unexpectedencounter()
-    coralsegments()
-
+    octo()
+    #coralsegments()
